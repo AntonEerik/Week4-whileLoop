5c1910731893c79706b28e3f21b4f70d96751aca
Anton Eerik Pauts <anton.eerik.pauts@gmail.com>
2021-10-06 17:14:21 +0300

    commit

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis on ta mängu võitnud;
+            //katsete arv on piiramatu;
+            //*programm genereerib juhuslikku numbrit ühe korra;
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            Console.WriteLine("Arva number 1-10-ni.");
+
+            while (true)
+            {
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber != cpuNumber)
+                {
+                    Console.WriteLine("Vale number! Proovi uuesti!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Õige number! Võitsid mängu!");
+            
+        }
+    }
+}
---
+++ GuessingGameThreeTimes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGameThreeTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis on ta mängu võitnud;
+            //katsete arv 3;
+            //*programm genereerib juhuslikku numbrit ühe korra;
+            
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            int i = 3;
+            Console.WriteLine($"Arva number 1-10-ni. Sul on {i} katset.");
+
+            while (i>0)
+            {
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber != cpuNumber)
+                {
+                    i--;
+                    Console.WriteLine($"Vale number! Proovi uuesti! Sul on jäänud {i} katset.");
+                }
+                else
+                {
+                    Console.WriteLine("Õige number! Võitsid mängu!");
+                    break;
+                }
+            }
+            if (i==0)
+            {
+                Console.WriteLine("Kaotasid mängu!");
+            }
+            
+            
+        }
+    }
+}
